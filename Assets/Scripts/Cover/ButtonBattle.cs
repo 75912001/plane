d7b803de83c0d72db6f2455d154d9002bf53d409
75912001 <75912001@qq.com>
2018-05-29 23:32:29 +0800

    do

---
+++ Assets/Scripts/Cover/ButtonBattle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBattleEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button btn = this.GetComponent<Button> ();
+		UIEventListener btnListener = btn.gameObject.AddComponent<UIEventListener> ();
+		btnListener.OnClick += delegate(GameObject gb) {
+			Debug.Log(gb.name + " OnClick");
+		};
+
+		btnListener.OnMouseEnter += delegate(GameObject gb) {
+			Debug.Log(gb.name + " OnMouseEnter");
+		};
+
+		btnListener.OnMouseExit += delegate(GameObject gb) {
+			Debug.Log(gb.name + " OnMOuseExit");
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
