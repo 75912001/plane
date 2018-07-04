ceae5cbe9ba4a3de19d125f5918316edde2bdc88
75912001@qq.com <75912001@qq.com>
2018-07-04 18:06:04 +0800

    win

--- Assets/Scripts/Btl/BtlFire.cs
+++ Assets/Scripts/Btl/BtlFire.cs
@@ -48,9 +48,30 @@ public class BtlFire : MonoBehaviour {
             Vector3 newBulletPosition = new Vector3();
             newBulletPosition.x = transform.position.x + bullet.xmlBullet.positionOffsetX;
             newBulletPosition.y = transform.position.y + bullet.xmlBullet.positionOffsetY;
-            
+
+            #region 计算出子弹到目标飞机的角度
+            BtlPlane userPlane = Global.Instance.btlMgr.GetUserPlane();
+            float x = (userPlane.gameObject.transform.position.x - newBulletPosition.x);
+            float y = -(userPlane.gameObject.transform.position.y - newBulletPosition.y);
+            if (System.Math.Abs(y) < System.Math.Abs(bullet.xmlBullet.directionOffsetY))
+            {
+                y = bullet.xmlBullet.directionOffsetY;
+            }
+            #endregion
+
+            #region 旋转子弹
+            float angleOfLine = (float)(System.Math.Atan2((x), (y)) * 180 / System.Math.PI);
+            if (!is_user_plane)
+            {
+                angleOfLine -= 180.0f;
+            }
+            Vector3 rotation = new Vector3(0, 0, angleOfLine);
+            Quaternion quaternion = new Quaternion();
+            quaternion = Quaternion.Euler(rotation);
+            #endregion
+
 			//计算子弹的偏移量，得出发射的初始位置
-            newBullet.gameObject = Instantiate(bulletPrefabs, newBulletPosition, transform.rotation);
+            newBullet.gameObject = Instantiate(bulletPrefabs, newBulletPosition, quaternion);
             // newBullet.gameObject.transform.LookAt(plane.gameObject.transform);
 			#region 此处设置,飞机销毁,子弹也会随父节点销毁
 			//bulletPrefab.transform.SetParent(plane.gameObject.transform);
@@ -74,17 +95,6 @@ public class BtlFire : MonoBehaviour {
                 newBullet.btlMove.direction = new Vector2(bullet.xmlBullet.directionOffsetX, bullet.xmlBullet.directionOffsetY);
             } else {
                 newBullet.gameObject.layer = (int)EnumLayer.EnemyBullet;
-
-                #region 计算出子弹到目标飞机的角度
-                BtlPlane userPlane = Global.Instance.btlMgr.GetUserPlane();
-                float x = (userPlane.gameObject.transform.position.x - newBullet.gameObject.transform.position.x);
-                float y = -(userPlane.gameObject.transform.position.y - newBullet.gameObject.transform.position.y);
-                if (System.Math.Abs(y) < System.Math.Abs(bullet.xmlBullet.directionOffsetY))
-                {
-                    y = bullet.xmlBullet.directionOffsetY;
-                }
-                #endregion
-
                 newBullet.btlMove.direction = new Vector2(x * bullet.xmlBullet.directionOffsetX, y * bullet.xmlBullet.directionOffsetY);
             }
         }
